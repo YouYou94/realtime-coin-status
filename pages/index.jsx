dd455a62993d5dafa3026606deffc73015775e9a
YouYou94 <ybh4115@gmail.com>
2022-12-06 15:35:50 +0900

    Add: 검색창 아이콘 만들기

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image.js";
 import Seo from "../components/Seo/Seo.jsx";
 import * as Styled from "../styles/PagesStyled/indexStyled.jsx";
 
@@ -5,7 +6,12 @@ export default function Home() {
   return (
     <Styled.IndexLayout>
       <Seo title={"Home"} />
-      안녕~
+      <Styled.SearchBox>
+        <Styled.Text>Realtime Coin Status</Styled.Text>
+        <Styled.IconBox>
+          <Image src={"/search.png"} alt="Search Icon" width={25} height={25} />
+        </Styled.IconBox>
+      </Styled.SearchBox>
     </Styled.IndexLayout>
   );
 }
--- styles/PagesStyled/indexStyled.jsx
+++ styles/PagesStyled/indexStyled.jsx
@@ -3,4 +3,31 @@ import styled from "styled-components";
 export const IndexLayout = styled.div`
   max-width: 45rem;
   min-width: 30rem;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+export const SearchBox = styled.div`
+  height: 5rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Text = styled.span`
+  margin: 0 1rem;
+  font-family: "GowunDodum";
+  font-size: 1.3rem;
+
+  color: white;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const IconBox = styled.div`
+  cursor: pointer;
 `;
