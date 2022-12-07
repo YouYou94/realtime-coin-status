09ab44ce258fdbcf353495f5bc999f9338244fe0
YouYou94 <ybh4115@gmail.com>
2022-12-07 16:57:13 +0900

    Mod: HomeIndex Styled 수정

--- styles/PagesStyled/indexStyled.jsx
+++ styles/PagesStyled/indexStyled.jsx
@@ -6,28 +6,70 @@ export const IndexLayout = styled.div`
 
   display: flex;
   flex-direction: column;
+  align-items: center;
+`;
+
+export const Title = styled.h2`
+  font-family: "GowunDodum";
+
+  color: white;
+`;
+
+export const EmphasisTitle = styled.em`
+  margin-right: 0.2rem;
+
+  font-size: 2rem;
 `;
 
 export const SearchBox = styled.div`
-  height: 5rem;
+  position: relative;
+
+  height: 2rem;
 
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
 
-export const Text = styled.span`
-  margin: 0 1rem;
-  font-family: "GowunDodum";
-  font-size: 1.3rem;
+export const SearchInput = styled.input`
+  width: 25rem;
+  height: 2rem;
 
-  color: white;
+  padding: 0 1rem;
+  border: none;
+
+  outline: none;
+
+  transition: all 0.35s;
+`;
+
+export const ResultList = styled.ul`
+  position: absolute;
+
+  width: 100%;
+
+  top: 2rem;
+
+  margin: 0;
+  padding-left: 1rem;
+  border-top: 1px solid black;
+
+  background-color: white;
+
+  list-style: none;
+`;
+
+export const ResultItem = styled.li`
+  width: 100%;
+
+  font-family: "GowunDodum";
+
+  cursor: pointer;
 
   &:hover {
-    text-decoration: underline;
+    background-color: rgb(250, 250, 250);
   }
 `;
 
-export const IconBox = styled.div`
-  cursor: pointer;
-`;
+export const RankingBox = styled.div``;
