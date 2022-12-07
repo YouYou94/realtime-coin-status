9a9353ee964041096bf4e3a3a71c8dc33f1961cc
YouYou94 <ybh4115@gmail.com>
2022-12-07 16:56:43 +0900

    Feat: SSR로 코인 데이터 불러와 검색 기능 만들기

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,62 @@
-import Image from "next/image.js";
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo/Seo.jsx";
 import * as Styled from "../styles/PagesStyled/indexStyled.jsx";
 
-export default function Home() {
+export default function Home({ coinList }) {
+  const [search, setSearch] = useState("");
+  const [resultList, setResultList] = useState([]);
+
+  const handleOnChangeSearch = (event) => {
+    const { value } = event.target;
+
+    setSearch(value);
+
+    let searchList = coinList
+      .filter((coin) => {
+        return coin.name.toUpperCase().includes(value.toUpperCase());
+      })
+      .map((coin) => {
+        const { id, name } = coin;
+        return <Styled.ResultItem key={id}>{name}</Styled.ResultItem>;
+      });
+
+    if (searchList.length > 10) searchList = searchList.slice(0, 10);
+    else searchList = searchList.slice(0, searchList.length);
+
+    if (value) {
+      setResultList(searchList);
+    } else setResultList([]);
+  };
+
   return (
     <Styled.IndexLayout>
       <Seo title={"Home"} />
+      <Styled.Title>
+        <Styled.EmphasisTitle>R</Styled.EmphasisTitle>ealtime{" "}
+        <Styled.EmphasisTitle>C</Styled.EmphasisTitle>oin{" "}
+        <Styled.EmphasisTitle>S</Styled.EmphasisTitle>tatus
+      </Styled.Title>
       <Styled.SearchBox>
-        <Styled.Text>Realtime Coin Status</Styled.Text>
-        <Styled.IconBox>
-          <Image src={"/search.png"} alt="Search Icon" width={25} height={25} />
-        </Styled.IconBox>
+        <Styled.SearchInput
+          type="text"
+          value={search}
+          onChange={handleOnChangeSearch}
+        />
+        <Styled.ResultList>{resultList}</Styled.ResultList>
       </Styled.SearchBox>
+      <Styled.RankingBox></Styled.RankingBox>
     </Styled.IndexLayout>
   );
+}
+
+export async function getServerSideProps() {
+  const coinList = await await (
+    await fetch("https://api.coinpaprika.com/v1/tickers?quotes=KRW")
+  ).json();
+
+  return {
+    props: {
+      coinList,
+    },
+  };
 }
