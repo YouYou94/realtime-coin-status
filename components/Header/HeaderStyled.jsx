9d4941defdb8fa0307b99d9653dd781400d225dd
YouYou94 <ybh4115@gmail.com>
2022-12-09 17:29:31 +0900

    Add: Header 추가

---
+++ components/Header/HeaderStyled.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const HeaderLayout = styled.div`
+  height: 5rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 2rem;
+
+  background-color: rgb(9, 52, 131);
+`;
+
+export const HeaderTitle = styled.h2`
+  font-family: "GowunDodum";
+
+  color: white;
+`;
