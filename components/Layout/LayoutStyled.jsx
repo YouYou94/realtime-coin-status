09b81ecab8ba6222443fde161b7f9e78481aca99
YouYou94 <ybh4115@gmail.com>
2022-12-09 16:04:35 +0900

    Mod: Layout Styled 배경색 변경

--- components/Layout/LayoutStyled.jsx
+++ components/Layout/LayoutStyled.jsx
@@ -5,8 +5,6 @@ export const Layout = styled.div`
   height: 100vh;
 
   display: flex;
-  justify-content: center;
-  align-items: center;
 
-  background-color: rgb(9, 52, 131);
+  background-color: rgb(246, 246, 246);
 `;
