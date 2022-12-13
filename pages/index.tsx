0d3f89cb4dec455e1dde47c962ce0f0b0594b9ae
YouYou94 <ybh4115@gmail.com>
2022-12-13 21:35:36 +0900

    Add: Seo tsx 변환 및 index에 export

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import styled from "styled-components"
+import Seo from "../components/Seo/Seo";
 
 export default function Home() {
-    return <IndexLayout></IndexLayout>
+    return (
+    <IndexLayout>
+        <Seo title='Home' />
+    </IndexLayout>)
 }
 
 export const IndexLayout = styled.div`
