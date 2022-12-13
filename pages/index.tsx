9ec0794896fa40596fad829ba0f3890283e302ec
YouYou94 <ybh4115@gmail.com>
2022-12-13 21:30:01 +0900

    Add: index tsx 변환

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export default function Home() {
+    return <IndexLayout></IndexLayout>
+}
+
+export const IndexLayout = styled.div`
+    flex: 1;
+
+    display: flex;
+    flex-direction: column;
+`;
