9c61984911d0182b2599930970bc028f41093d46
YouYou94 <ybh4115@gmail.com>
2022-12-14 12:25:56 +0900

    feat: SeverSideProps로 코인 데이터 받아오기

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
 import styled from 'styled-components';
 import Seo from '../components/Seo/Seo';
 
-export default function Home() {
+export default function Home({ coinList }: any) {
   return (
     <IndexLayout>
       <Seo title="Home" />
     </IndexLayout>
   );
+}
+
+export async function getServerSideProps() {
+  const coinList = await (
+    await (await fetch('http://localhost:3000/api/coins')).json()
+  ).slice(0, 100);
+
+  return {
+    props: {
+      coinList,
+    },
+  };
 }
 
 export const IndexLayout = styled.div`
